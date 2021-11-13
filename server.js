3c6fe8b50b078c587b59abe79699810c5be767b1
Zach Smith <Lemelisk27@gmail.com>
2021-11-13 10:49:42 -0800

    added db connections for heroku

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+  );
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
